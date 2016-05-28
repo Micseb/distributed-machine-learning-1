5ffa79e1f55d5d668e88e4800efce486de1d5524
Santiago Morante, PhD <smorante@users.noreply.github.com>
2016-05-28 13:18:22 +0200

    first version using signal class

--- DMF/DMF.py
+++ DMF/DMF.py
@@ -1,24 +1,66 @@
-# Databricks notebook source exported at Fri, 27 May 2016 08:43:12 UTC
+# Databricks notebook source exported at Sat, 28 May 2016 11:16:47 UTC
 """
 Author: Santiago Morante
+DISTRIBUTED ANOMALY DETECTION ALGORITHM BASED ON DISSIMILARITY MAPPING FILTERING
+S. Morante, J. G. Victores and C. Balaguer, "Automatic demonstration and feature selection for robot learning," 
+Humanoid Robots (Humanoids), 2015 IEEE-RAS 15th International Conference on, Seoul, 2015, pp. 428-433.
+doi: 10.1109/HUMANOIDS.2015.7363569
+URL: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7363569&isnumber=7362951
 """
 
 from pyspark.sql import DataFrame as DFPyspark
-from pyspark.rdd import RDD
 from pyspark.sql import SQLContext
+from pyspark.rdd import RDD
 from pyspark import SparkContext
 from pandas import DataFrame as DFPandas
 from numpy.linalg import norm
+from fastdtw import fastdtw
 
+class Signal():
+  """
+  Signal:
+  1. Takes data in regular table style (rows, columns), 
+  2. Converts it to RDD (if not yet)
+  3. Indexes every row with the index in the first position of a tupla (index, listOfValuesOfTheRow),
+  4. Imputes values in missing positions. --toDo
+  
+  Each tupla in a Signal instance represents a vector of data.
+  """
+
+  def create(self, dataset):
+    """Creates Signal from list, rdd, dataframe (spark) or dataframe (pandas)"""
+    rdd = self.convertToRDD(dataset)
+    return self.indexRDD(rdd)
+    
+  def convertToRDD(self, dataset):
+    """Converts dataset into RDD (if not yet)"""
+    sc = SparkContext.getOrCreate()
+    if isinstance(dataset, RDD):
+      return dataset
+    elif isinstance(dataset, DFPyspark):
+        return dataset.rdd.map(list)
+    elif isinstance(dataset, DFPandas):
+      sqlContext = SQLContext.getOrCreate(sc)
+      return sqlContext.createDataFrame(dataset).rdd.map(list)
+    else:
+      try:
+          return sc.parallelize(dataset)
+      except TypeError:
+          print("convertToRDD cannot convert dataset because it is not one of the allowed types \
+          (RDD, dataframe (sparkSQL) or dataframe (pandas))")
+  
+  def indexRDD(self, rdd):
+    """Index each row of a RDD as a tuple (index, listOfValuesOfTheRow)"""
+    return rdd.zipWithIndex().map(lambda x: (x[1],x[0]))
+  
+#######################################################################  
 
 class DMF():
     """
-    DISTRIBUTED ANOMALY DETECTION ALGORITHM BASED ON DISSIMILARITY MAPPING FILTERING
+    DISSIMILARITY MAPPING FILTERING:
     S. Morante, J. G. Victores and C. Balaguer, "Automatic demonstration and feature selection for robot learning," 
     Humanoid Robots (Humanoids), 2015 IEEE-RAS 15th International Conference on, Seoul, 2015, pp. 428-433.
-    doi: 10.1109/HUMANOIDS.2015.7363569
-    URL: http://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7363569&isnumber=7362951
-
+    
     :param data:                 RDD of data points
     :param dissimilarityMethod:  Method for performing dissimilarity
     :param mappingMethod:        Method for performing mapping
@@ -27,34 +69,31 @@ class DMF():
     """
    
     def __init__(self, dissimilarityMethod="euclidean", mappingMethod="sum", filteringMethod="zscore", theta=0):
+      """Initialize parameters"""
       self.dissimilarityMethod = dissimilarityMethod
       self.mappingMethod = mappingMethod
       self.filteringMethod = filteringMethod
-      self.theta=theta
+      self.theta = theta
 
-    def convertToRDD(self, dataset):
-      sc = SparkContext.getOrCreate()
-      if isinstance(dataset, RDD):
-        return dataset
-      
-      elif isinstance(dataset, DFPyspark):
-          return dataset.rdd.flatMap(list)
-      
-      elif isinstance(dataset, DFPandas):
-        sqlContext = SQLContext.getOrCreate(sc)
-        return sqlContext.createDataFrame(dataset).rdd.flatMap(list)
-      
-      else:
-        try:
-            return sc.parallelize(dataset)
-        except TypeError:
-            print("convertToRDD cannot convert your dataset because it is not one of the allowed types (RDD, dataframe (sql) or dataframe(pandas))")
- 
-      
     def dissimilarity(self, doublePairs): 
       """DISSIMILARITY: calculate distance between elements"""
+      #euclidean
       if self.dissimilarityMethod == "euclidean":
-        return doublePairs[0][0], norm(doublePairs[0][1]-doublePairs[1][1])
+        #convert to float
+        element0 = [float(x) for x in doublePairs[0][1]]
+        element1 = [float(x) for x in doublePairs[1][1]]
+        #subtract lists
+        subtraction = [a - b for a, b in zip(element1, element0)]
+        return float(doublePairs[0][0]), norm(subtraction)
+      #dtw
+      if self.dissimilarityMethod == "dtw":
+        #convert to float
+        element0 = [float(x) for x in doublePairs[0][1]]
+        element1 = [float(x) for x in doublePairs[1][1]]
+        #fastdtw
+        distance, path = fastdtw(element0, element1)
+        return float(doublePairs[0][0]), distance
+      #unknown
       else:
         raise ValueError("dissimilarityMethod should be an allowed method, "
                             "but got %s." % str(dissimilarityMethod))
@@ -66,38 +105,29 @@ class DMF():
       else:
         raise ValueError("mappingMethod should be an allowed method, "
                               "but got %s." % str(mappingMethod))
-
+        
     def filtering(self, pairs, meanData, stdevData, theta):
       """FILTERING: filtering data by threshold (theta) using Z-score"""
       if self.filteringMethod == "zscore":
-        return (pairs[1] - meanData)/float(stdevData) > theta 
+        try:
+          return (pairs[1] - meanData)/float(stdevData) > theta 
+        except:
+          raise ValueError("Filtering Z-score division by zero (StDev=0)!  (all values are equal)")
       else:
          raise ValueError("filteringMethod should be an allowed method, "
                               "but got %s." % str(filteringMethod))
 
-    def detect(self, dataset):
-      """Detects anomalies in the dataset"""
- 
-      #convert to RDD
-      rdd = self.convertToRDD(dataset)
-      
-      #index data
-      dataIndex = rdd.zipWithIndex().map(lambda x: (x[1],x[0]))
-      
+    def detect(self, signals):
+      """Detects anomalies in the dataset (must be in format (index,listOfValues))"""
       #dissimilarity
-      dataDissimilarity = dataIndex.cartesian(dataIndex).map(self.dissimilarity)
-      
+      dataDissimilarity = signals.cartesian(signals).map(self.dissimilarity)
       #mapping
       dataMapping = dataDissimilarity.reduceByKey(self.mapping)
-      
       #filtering
       meanData= dataMapping.values().mean()
       stdevData= dataMapping.values().stdev()
       dataFiltering = dataMapping.filter(lambda pairs: self.filtering(pairs, meanData, stdevData, self.theta))
-      
-      #return
       return dataFiltering
-  
 
 # COMMAND ----------
 
@@ -111,32 +141,18 @@ from pyspark import SparkContext
 
 #sparkContext
 sc = SparkContext.getOrCreate()
+sqlContext = SQLContext.getOrCreate(sc)
+data=sqlContext.read.table("dim2small")
 
-#DATA: generate sine wave with outliers
-data = sin(range(0,10)).tolist() + \
-                    [35] +  \
-                    sin(range(11,20)).tolist() + \
-                    [32]  +  \
-                    sin(range(21,30)).tolist() + \
-                    [60]  +  \
-                    sin(range(31,50)).tolist() 
-
-#plot
-x = linspace(0, len(data))
-fig, ax = plt.subplots()
-plt.plot(x, data, '-', linewidth=2)
-display(fig)
 
 # COMMAND ----------
 
-#parallelize data
-dataParallel = sc.parallelize(data)    
-
 #create model
-model= DMF()
+model= DMF(dissimilarityMethod="dtw")
+signals = Signal().create(data)
 
 #detect using model
-print ("Index, totalValue: ", model.detect(data).collect())
+print ("Index, totalValue: ", model.detect(signals).collect())
 
 # COMMAND ----------
 
